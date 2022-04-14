658b20b1c1dfbe6d4f35310f7beda2838a4a6c5d
p.pozhidaev <raspasyan8436@yandex.ru>
2022-04-14 11:05:46 +1000

    Убран хлам из роутера.

--- app/php/bonusapp.class.php
+++ app/php/bonusapp.class.php
@@ -48,21 +48,8 @@ class BonusApp {
             }
 
             case "": {
-                // $valid_passwords = array ("2020" => "123");
-                // $valid_users = array_keys($valid_passwords);
-
-                // $user = $_SERVER['PHP_AUTH_USER'];
-                // $pass = $_SERVER['PHP_AUTH_PW'];
-
-                // $validated = (in_array($user, $valid_users)) && ($pass == $valid_passwords[$user]);
-
-                // if (!$validated) {
-                //   header('WWW-Authenticate: Basic realm="My Realm"');
-                //   header('HTTP/1.0 401 Unauthorized');
-                //   die ("Not authorized");
-                // }
-
                 require_once 'templates/index.html';
+                
                 break;
             }
 
