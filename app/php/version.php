6fca7227214d2fba17134d6f3a00a629254df2bc
p.pozhidaev <raspasyan8436@yandex.ru>
2022-05-23 14:19:19 +1000

    Обновлена версия для приложений обеих платформ.

--- app/php/version.php
+++ app/php/version.php
@@ -1,4 +1,4 @@
 <?php
     define("APP_VERSION", "APP_216");
-    define("APP_VERSION_ANDROID", "216");
-    define("APP_VERSION_IOS", "217");
+    define("APP_VERSION_ANDROID", "218");
+    define("APP_VERSION_IOS", "218");
