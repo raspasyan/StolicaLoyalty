f81632bb9bca7dd00af4dc9e22e60b188489f92c
d.grebenuk <d.grebenuk@RSA-404-05.rsa.local>
2022-05-24 15:08:02 +1000

    Исправлено определение браузера на iPhone и iPod

--- app/src/js/module.main.js
+++ app/src/js/module.main.js
@@ -106,7 +106,7 @@ const deviceType = () => {
 
     if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
         return "mobile";
-    } else if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
+    } else if (/Mobile|Android|iPhone|iPad|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)) {
         return "mobile";
     }
 
