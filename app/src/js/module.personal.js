300af4519cfc42b388b63b8c5ba51570958b9aab
d.grebenuk <d.grebenuk@RSA-404-05.rsa.local>
2022-06-20 17:22:25 +1000

    Добавлен сканер qrкода для android

--- app/src/js/module.personal.js
+++ app/src/js/module.personal.js
@@ -221,12 +221,48 @@ let loadScanerScript = false;
 let scanerIsEnable   = false;
 
 C("#scanerQR").el.addEventListener("click", () => {
-    if (loadScanerScript) {
-        startScaner();
+    if (PLATFORM) { 
+        document.addEventListener("deviceready", function () {
+            switch (device.platform) {
+                case "Android":
+                    cordova.plugins.barcodeScanner.scan(
+                       function (result) {
+                            C("#plasticNumber").el.value = result.text;
+                            setCard();
+                       },
+                       function (error) {
+                           //alert("Scanning failed: " + error);
+                       },
+                       {
+                           preferFrontCamera : false, // iOS and Android
+                           showFlipCameraButton : true, // iOS and Android
+                           showTorchButton : true, // iOS and Android
+                           torchOn: false, // Android, launch with the torch switched on (if available)
+                           saveHistory: true, // Android, save scan history (default false)
+                           prompt : "Просканируйте QR код", // Android
+                           resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+                           formats : "QR_CODE", // default: all but PDF_417 and RSS_EXPANDED
+                           orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+                           disableAnimations : true, // iOS
+                           disableSuccessBeep: false // iOS and Android
+                       }
+                    );
+
+                    break;
+                case "iOS":
+
+                    break;
+            }
+        });
     } else {
-        loadScaner();
+        if (loadScanerScript) {
+            startScaner();
+        } else {
+            loadScaner();
+        }
     }
 });
+
 
 function loadScaner() {
     showLoader();
